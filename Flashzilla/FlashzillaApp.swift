bcbdf3e6eb264c0a2de4d4748de85c50bf959e6c
Héctor Sandoval <ingsan@live.com.mx>
2023-12-13 12:00:51 -0600

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Héctor Manuel Sandoval Landázuri on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
